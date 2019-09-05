b72a08d78781bb8f850a656f9732b46e955a5670
SebaAlex12 <sebastiank24@wp.pl>
2019-09-05 19:11:12 +0200

    what to do on specyfic routes

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express = require("express");
 import mongoose = require("mongoose");
 import bodyParser = require("body-parser");
 import multer = require("multer");
+const path = require("path");
 
 import graphqlHttp = require("express-graphql");
 import graphqlSchema = require("./graphql/schema");
@@ -119,3 +120,12 @@ app.use(
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
+
+// serv assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
