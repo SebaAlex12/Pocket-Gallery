efa14072dae3a90208c0fd9f480546f5cb3f9502
SebaAlex12 <sebastiank24@wp.pl>
2019-09-19 12:29:19 +0200

    heroku

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose = require("mongoose");
 import express = require("express");
 import bodyParser = require("body-parser");
 import path = require("path");
